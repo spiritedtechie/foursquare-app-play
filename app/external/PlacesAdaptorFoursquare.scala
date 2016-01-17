f956189684f1465f6bd0877fbc94e4a5921fb03a
Vijay Patel <vijay@spiritedtechie.com>
2016-01-17 17:51:15 +0000

    Refactoring - extracted out function to map json to places. Made handlingJsonResponseCode a higher order function (passing in the map function).  Used Try (rather than throwing exception) in these functions so that caller is forced to handle any failure and to be more in line with functional style. Also created function to create venues request.

--- app/external/PlacesAdaptorFoursquare.scala
+++ app/external/PlacesAdaptorFoursquare.scala
@@ -12,10 +12,13 @@ import services.PlacesService
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
 class PlacesAdaptorFoursquare @Inject()(ws: WSClient, configuration: Configuration) extends PlacesService {
 
   private lazy val foursquareBaseUrl = configuration.getString("app.foursquare.baseUrl").getOrElse("Unknown")
+
+  private lazy val foursquareExploreVenuesUrl = s"${foursquareBaseUrl}/v2/venues/explore"
 
   private implicit val locationReads: Reads[Location] =
     (
@@ -37,30 +40,36 @@ class PlacesAdaptorFoursquare @Inject()(ws: WSClient, configuration: Configurati
       (JsPath \ "venue" \ "rating").readNullable[BigDecimal]
       )(Place.apply _)
 
-  private def handlingJsonResponseCode(json: JsValue) = {
-    (json \ "meta" \ "code").toOption match {
-      case Some(JsNumber(code)) if code == 200 => {
-        val d = ((json \ "response" \ "groups")(0) \ "items").validate[Seq[Place]]
+  private def requestForPopularVenuesNear(name: String)(url: String) =
+    ws.url(url)
+      .withQueryString("near" -> name)
+      .withQueryString("client_id" -> "G13G1JGM3EA4FXORGPFBMPGBDSBMYMR1S1KYV2OAUF33PQV4")
+      .withQueryString("client_secret" -> "ADGLVHHMGASP4RRKDY0O2UKQZKUD1XLLJV505M0JJ3A4LSB2")
+      .withQueryString("v" -> "20150113")
 
-        d match {
-          case s: JsSuccess[Seq[Place]] => Some(s.value)
-          case e: JsError => throw new PlacesRetrievalException("Failure when parsing data from Foursquare")
-        }
-      }
-      case Some(JsNumber(code)) if code == 400 => None
+  private val tryMapToPlaces = (json: JsValue) => Try {
+    ((json \ "response" \ "groups")(0) \ "items").validate[Seq[Place]] match {
+      case s: JsSuccess[Seq[Place]] => Some(s.value)
+      case e: JsError => throw new PlacesRetrievalException("Failure when parsing data from Foursquare")
+    }
+  }
+
+  private def handlingJsonResponseCode(json: JsValue)
+                                      (tryMapJsonToPlaces: JsValue => Try[Some[Seq[Place]]]) = {
+    (json \ "meta" \ "code").toOption match {
+      case Some(JsNumber(code)) if code == 200 => tryMapJsonToPlaces(json)
+      case Some(JsNumber(code)) if code == 400 => Try(None)
       case _ => throw new PlacesRetrievalException("Failure when finding places from Foursquare")
     }
   }
 
   override def findPlacesNear(name: String): Future[Option[Seq[Place]]] = {
 
-    val request =
-      ws.url(s"${foursquareBaseUrl}/v2/venues/explore")
-        .withQueryString("near" -> name)
-        .withQueryString("client_id" -> "G13G1JGM3EA4FXORGPFBMPGBDSBMYMR1S1KYV2OAUF33PQV4")
-        .withQueryString("client_secret" -> "ADGLVHHMGASP4RRKDY0O2UKQZKUD1XLLJV505M0JJ3A4LSB2")
-        .withQueryString("v" -> "20150113")
-
-    request.get().map { response => handlingJsonResponseCode(response.json) }
+    requestForPopularVenuesNear(name)(foursquareExploreVenuesUrl).get().map {
+      response => handlingJsonResponseCode(response.json)(tryMapToPlaces) match {
+        case Success(p) => p
+        case Failure(e) => throw e
+      }
+    }
   }
 }
