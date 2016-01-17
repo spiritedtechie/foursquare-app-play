2b251bb7269d29d3b888113c32ff450c21d3a6fa
Vijay Patel <vijaypatel@eurocarparks.com>
2016-01-17 16:36:14 +0000

    Added richer data to places search stub result for a better demo from the web app.

--- app/services/PlacesServiceStub.scala
+++ app/services/PlacesServiceStub.scala
@@ -1,12 +1,17 @@
 package services
 
-import models.Place
+import models.{Contact, Location, Place}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class PlacesServiceStub extends PlacesService {
   override def findPlacesNear(name: String) = Future {
-    Some(Seq(new Place("Awesome place")))
+    Some(
+      Seq(
+        Place("Hampstead Heath", Some("http://www.cityoflondon.gov.uk/hampstead"), Some(Location(Some("E Heath Rd"), Some("London"), Some("United Kingdom"), Some("NW3 2PT"))), Some(Contact(Some("02073323322"))), Some(9.7)),
+        Place("Hyde Park", Some("http://www.royalparks.org.uk/parks/hyde-park"), Some(Location(Some("Serpentine Rd"), Some("London"), Some("United Kingdom"), Some("W2 2TP"))), Some(Contact(None)), Some(9.6))
+      )
+    )
   }
 }
